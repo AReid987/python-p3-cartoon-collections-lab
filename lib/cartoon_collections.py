d9d85f17f2685e749a76103be55ff68847ddd6d2
Antonio Reid <read.musik@gmail.com>
2023-01-27 10:15:08 -0600

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i, name in enumerate(names):
+      print(f"{i + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    excited_calls = [f"{call.capitalize()}!" for call in calls]
+    return excited_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
 
-def find_the_cheese():
-    pass
+    result = False
+    for call in calls: 
+        if len(call) > 4: 
+            result = True
+            break;
+
+    return result
+
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    result = None
+    for cheese in cheeses: 
+        if cheese in snacks:
+          result = cheese
+          break;
+
+    return result
